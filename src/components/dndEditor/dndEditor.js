a83f08e4732e42087dd53b74c98d449edb533681
Paul Ayling <36618373+PaulAyling@users.noreply.github.com>
2019-11-01 07:39:57 +0200

    Created new component dndEditor and moved & updated files to work in a compent directory

---
+++ src/components/dndEditor/dndEditor.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '@atlaskit/css-reset';
+import 'materialize-css/dist/css/materialize.min.css';
+import '../../index.css'
+
+import styled from 'styled-components';
+import { DragDropContext } from 'react-beautiful-dnd';
+import initialData from './initialData';
+import Column from './column';
+
+
+const Container = styled.div`
+  display: flex;
+`;
+
+export default class App extends React.Component {
+  state = initialData;
+
+  onDragEnd = result => {
+    document.body.style.color = 'inherit';
+    const { destination, source, draggableId } = result;
+    if (!destination) {
+      return;
+    }
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const start = this.state.columns[source.droppableId];
+    const finish = this.state.columns[destination.droppableId];
+
+    if (start === finish) {
+      const newTaskIds = Array.from(start.taskIds);
+      newTaskIds.splice(source.index, 1);
+      newTaskIds.splice(destination.index, 0, draggableId);
+
+      const newColumn = {
+        ...start,
+        taskIds: newTaskIds
+      };
+      const newState = {
+        ...this.state,
+        columns: {
+          ...this.state.columns,
+          [newColumn.id]: newColumn
+        }
+      };
+      this.setState(newState);
+      return;
+    }
+    //Start and finnish columns are different
+    const startTaskIds = Array.from(start.taskIds);
+    startTaskIds.splice(source.index, 1);
+    const newStart = {
+      ...start,
+      taskIds: startTaskIds
+    };
+
+    const finishTaskIds = Array.from(finish.taskIds);
+    finishTaskIds.splice(source.index, 0, draggableId);
+    const newFinish = {
+      ...finish,
+      taskIds: finishTaskIds
+    };
+
+    const newState = {
+      ...this.state,
+      columns: {
+        ...this.state.columns,
+        [newStart.id]: newStart,
+        [newFinish.id]: newFinish
+      }
+    };
+    this.setState(newState);
+    return;
+  };
+
+  render() {
+    return (
+      <Container>
+        <DragDropContext onDragEnd={this.onDragEnd}>
+          {this.state.columnOrder.map(columnId => {
+            const column = this.state.columns[columnId];
+            const tasks = column.taskIds.map(
+              taskId => this.state.tasks[taskId]
+            );
+
+            return <Column key={column.id} column={column} tasks={tasks} />;
+          })}
+        </DragDropContext>
+
+      </Container>
+    );
+  }
+}
+
+
---
+++ src/components/dndEditor/dndEditortest.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default class DndEditor extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>DND Editor </h1>
+      </div>
+    )
+  }
+}
