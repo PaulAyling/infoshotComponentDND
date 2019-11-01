d5497dd7fc3d1b982f5bd19deaa3b600f2cb030c
Paul Ayling <36618373+PaulAyling@users.noreply.github.com>
2019-11-01 09:48:50 +0200

    removed all erroneous files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '@atlaskit/css-reset';
-import 'materialize-css/dist/css/materialize.min.css';
-import './index.css';
-
 import styled from 'styled-components';
-import { DragDropContext } from 'react-beautiful-dnd';
-import initialData from './initial-data';
-import Column from './column';
 import DndEditor from './components/dndEditor/dndEditor';
 
 const Container = styled.div`
@@ -25,5 +18,4 @@ class App extends React.Component {
     );
   }
 }
-
 ReactDOM.render(<App />, document.getElementById('root'));
